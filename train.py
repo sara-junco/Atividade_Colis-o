b5911c02ea5c6d6f04bacc4883a1c4fb40a82c4e
Sarai junco <estefanijuncovallejo@gmail.com>
2026-04-05 21:45:23 -0300

    primeiro commit

--- train.py
+++ train.py
@@ -5,5 +5,12 @@ model = YOLO("yolov8n.pt")
 model.train(
     data="data.yaml",
     epochs=50,
-    imgsz=640
+    imgsz=640,
 )
+
+
+from ultralytics import YOLO
+
+model = YOLO("runs/detect/train/weights/best.pt")
+
+results = model("teste.jpg", show=True)
